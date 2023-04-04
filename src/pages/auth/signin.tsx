ad9ccbc36b7fa1c3ae58c8026be749b443e2f1d9
Angel Garcia <angelgarciaweb@gmail.com>
2023-04-04 15:54:45 -0400

    ✨ add signin with providers

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -1,23 +1,61 @@
 import type { GetServerSidePropsContext, InferGetServerSidePropsType } from "next"
-import { getProviders, signIn } from "next-auth/react"
+import { getProviders, signIn, useSession } from "next-auth/react"
 import { getServerSession } from "next-auth";
 import { authOptions } from "~/server/auth";
+import MainInput from "~/components/MainInput";
+import LoginIcon from "~/components/LoginIcons";
+import { useRouter } from "next/router";
+import Swal from "sweetalert2";
+import { useContext } from "react";
+import { LoadingContext } from "../_app";
+import Loader from "~/components/Loader";
+
+
 
 
 
 
 
 export default function SignIn({ providers }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+
   return (
     <>
-      {Object.values(providers).map((provider) => (
-          <div key={provider.name}>
-            <button onClick={() => signIn(provider.id)}>
-              Ingresa con: {provider.name}
-            </button>
+      
+      <main className="container mx-auto">
+        <Loader/>
+        <section className='flex justify-center items-center h-screen'>
+          <div className="relative items-center w-full px-5 mx-auto md:px-12 lg:px-20 max-w-7xl">
+            <div className="w-full max-w-md mx-auto md:max-w-sm md:px-0 md:w-96 sm:px-4">
+              <div className="flex flex-col">
+                <div>
+                  <h2 className="text-4xl text-black text-center">
+                    Sign In
+                  </h2>
+                </div>
+              </div>
+              <div>
+                <div className="mt-6 space-y-6">
+                  {Object.values(providers).map((provider) => (
+
+                    <div key={provider.name} className="col-span-full">
+                      {
+                        provider.name !== 'Credentials' && (
+                          <button onClick={() => signIn(provider.id)} type="button" className="inline-flex items-center justify-center w-full px-6 py-3 text-center text-black duration-200 bg-white border-2 border-black hover:bg-transparent hover:border-black rounded-xl hover:text-black focus:outline-none focus-visible:outline-black focus-visible:ring-black">
+                            <LoginIcon icon={provider.name} />
+                            With {provider.name}
+                          </button>
+                        )
+                      }
+                    </div>
+                  ))
+                  }
+                </div>
+              </div>
+            </div>
           </div>
-        ))
-      }
+        </section>
+      </main>
+
     </>
   )
 }
